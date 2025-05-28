9a6ec7c528b4c0f2a4b950cdfe04db2a88910156
Lucas <145467657+LucasPMLG@users.noreply.github.com>
2025-05-27 22:40:20 -0300

    Update IPtool.py

--- IPtool.py
+++ IPtool.py
@@ -1,13 +1,1 @@
-import socket
 
-def buscar_ip(nome_host):
-    try:
-        # Obtém o IP do nome do host
-        ip = socket.gethostbyname(nome_host)
-        print(f'O IP de {nome_host} é: {ip}')
-    except socket.gaierror:
-        print(f'Não foi possível encontrar o IP para o host: {nome_host}')
-
-# Exemplo de uso
-nome_host = input('Digite o nome do host para buscar o IP: ')
-buscar_ip(nome_host)
